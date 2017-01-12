1edfdcf06b2452a8bc5b819628b32a5f925ba9fd
meo3the <namtuocnam@gmail.com>
2017-01-13 01:02:50 +0700

    #2 Custom MainActivity screen, add Navigation drawer

--- app/src/main/java/com/m3t/music/MainActivity.java
+++ app/src/main/java/com/m3t/music/MainActivity.java
@@ -1,13 +1,69 @@
 package com.m3t.music;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.widget.RelativeLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.containMain)
+    RelativeLayout containMain;
+    @BindView(R.id.fabShuffle)
+    FloatingActionButton fabShuffle;
+    @BindView(R.id.navView)
+    NavigationView navView;
+    @BindView(R.id.drawer)
+    DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        init();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //  Inflate the menu
+        //  This adds items to the action bar if it present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void init() {
+        setSupportActionBar(toolbar);
+        if (getActionBar() != null) {
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+            getActionBar().setHomeButtonEnabled(true);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                    this, drawer, R.string.drawerOpen, R.string.drawerClose);
+            toggle.setDrawerIndicatorEnabled(true);
+            drawer.addDrawerListener(toggle);
+            toggle.syncState();
+        }
     }
 }
